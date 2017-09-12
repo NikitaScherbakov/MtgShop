b2e97b7240cd54a32c268b701f09554378de6bec
Nikita <red-dragon18@mail.ru>
2017-09-12 14:13:15 +0300

    initial

---
+++ src/main/java/Account.java
@@ -0,0 +1,63 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Accounts")
+public class Account implements Serializable {
+
+    private static final long serialVersionUID = -2054386655979281969L;
+
+
+    public static final String ROLE_ADMIN = "ADMIN";
+    public static final String ROLE_EMPLOYEE = "EMPLOYEE";
+
+    private String userName;
+    private String password;
+    private boolean active;
+    private String userRole;
+
+    @Id
+    @Column(name = "User_Name")
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Column(name = "Password")
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name = "Active")
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Column(name = "User_Role")
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    @Override
+    public String toString()  {
+        return "["+ this.userName+","+ this.password+","+ this.userRole+"]";
+    }
+}
